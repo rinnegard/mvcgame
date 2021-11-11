e59192306a0bd3eed0af3cc5a8981f91d992915c
Victor <victor.rinnegard@hotmail.com>
2021-11-11 01:58:13 +0000

    Added test for dice and dicehand

---
+++ test/Dice/DiceHandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace viri19\Dice;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Test cases for the Dice Class.
+ */
+class DiceHandTest extends TestCase
+{
+
+    public function testCreateDiceClass()
+    {
+        $dicehand = new DiceHand();
+        $this->assertIsArray($dicehand->getAllDice());
+    }
+
+    public function testRollDice()
+    {
+        $dicehand = new DiceHand(6);
+        $dicehand->roll();
+        $this->assertGreaterThan(6, $dicehand->getLastSum());
+    }
+
+
+}
---
+++ test/Dice/DiceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace viri19\Dice;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Test cases for the Dice Class.
+ */
+class DiceTest extends TestCase
+{
+
+    public function testCreateDiceClass()
+    {
+        $dice = new Dice();
+        $this->assertEquals(0, $dice->getFace());
+    }
+
+    public function testRollDice()
+    {
+        $dice = new Dice();
+        $dice->roll();
+        $this->assertGreaterThan(0, $dice->getFace());
+    }
+
+
+}
